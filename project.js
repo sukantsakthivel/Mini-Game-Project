987385460ab0ca2f44f321a3b509e65fcbdedfee
SUKANT S <sukantsakthivel66@gmail.com>
2023-03-24 16:31:00 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
-game();
-function game()
-{
+// game();
+// function game()
+// {
 let input = confirm("Can we play Rock,Paper,Scissors?");
 let no1 = "rock";
 let no2 = "paper";
@@ -8,7 +8,7 @@ let no3 = "scissors";
 if(input)
 {
     let player_input = prompt("Please Enter Rock or Paper or Scissor");
-    player_input = player_input.toLowerCase();
+    player_input = player_input.trim().toLowerCase();
     if(player_input)
     {
         if(player_input===no1 || player_input===no2 || player_input===no3)
@@ -22,19 +22,25 @@ if(input)
                 computer=no3;
             let winner = 
             computer===player_input
-            ?"Tie!"
+            ?`Player: ${player_input}
+Computer: ${computer}
+Tie!`
             :computer===no1 && player_input===no3 || computer===no2 && player_input===no1 || computer===no3 && player_input===no2 
-            ?"Computer Wins"
+            ?`Player: ${player_input}
+Computer: ${computer}
+Computer Wins`
             :player_input===no1 && computer===no3 || player_input===no2 && computer===no1 || player_input===no3 && computer===no2
-            ?"Player Wins"
-            :"Computer Wins";
+            ?`Player: ${player_input}
+Computer: ${computer}
+Player Wins`
+:"Computer Wins";
             
             alert(`${winner}`);
 
             let play_again = confirm("Shall we play again?")
             if(play_again)
             {
-                game();
+                location.reload();
             }
             else
             {
@@ -46,15 +52,17 @@ if(input)
         else
         {
             alert("Type properly in input box and refresh the page");
+            location.reload();
         }
     }
     else
     {
         alert("The input field is empty!");
+        location.reload();
     }
 }
 else
 {
     alert("Thank you");
 }
-}
+// }
